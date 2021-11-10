71d709df417f692262e495977e7b20e1dad19da8
Hung Nguyen <gaughegomth01@gmail.com>
2021-11-10 20:19:48 +0700

    [RUN] player

---
+++ Week01/Textures.h
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef _TEXTURES_H
+
+#include <unordered_map>
+#include <d3dx9.h>
+
+using namespace std;
+
+/*
+	Manage texture database
+*/
+
+class CTextures
+{
+	static CTextures* __instance;
+
+	unordered_map<int, LPDIRECT3DTEXTURE9> textures;
+
+public:
+	CTextures();
+	void Add(int id, LPCWSTR filePath, D3DCOLOR transparentColor);
+	LPDIRECT3DTEXTURE9 Get(unsigned int i);
+
+	static CTextures* GetInstance();
+};
+
+#endif // !_TEXTURES_H
+
+
